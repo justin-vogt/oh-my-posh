f8344fa127be8c472b279812a7eaccc70a5b83c2
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2021-09-18 18:59:24 +0200

    docs: add analytics

---
+++ docs/plugins/appinsights/analytics.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+export default (function () {
+  if (!ExecutionEnvironment.canUseDOM) {
+    return null;
+  }
+
+  return {
+    onRouteUpdate({ location }) {
+      window.appInsights.trackPageView({ name: location.pathname });
+    },
+  };
+})();
